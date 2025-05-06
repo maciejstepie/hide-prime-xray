1606f1a7eafe9198f2e6d8c1eaa4d0ae04bdb2cf
theSpawacz <maciej.stepien@kenja.com>
2025-05-06 13:16:54 +0200

    foramting

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # X-Ray Hider for Amazon Prime Video
 
-## Problem❗:
+## Problem ❗:
 
 ![what-it-does](/images/from.png)
 
-- Do you find the X-Ray overlay on Amazon Prime Video distracting when you pause a video and just want to focus on the subtitles and content? 🙄
-- This overlay can obscure important information, making it hard to enjoy your viewing experience.
+-   Do you find the X-Ray overlay on Amazon Prime Video distracting when you pause a video and just want to focus on the subtitles and content? 🙄
+-   This overlay can obscure important information, making it hard to enjoy your viewing experience.
 
 ## Solution ✅:
 
 ![what-it-does](/images/to.png)
 
-- This extension automatically hides the X-Ray overlay whenever you pause a video, allowing for clearer visibility of subtitles and content. ⏸️
-- Enjoy uninterrupted viewing without the distraction of unnecessary overlays! 🙌
+-   This extension automatically hides the X-Ray overlay whenever you pause a video, allowing for clearer visibility of subtitles and content. ⏸️
+-   Enjoy uninterrupted viewing without the distraction of unnecessary overlays! 🙌
--- content.js
+++ content.js
@@ -1,37 +1,37 @@
-const hideXrayQuickView = () => {
-  const xRayQuickViewEls = document.querySelectorAll(".xrayQuickView");
-  xRayQuickViewEls.forEach((xRayQuickViewEl) => {
-    if (xRayQuickViewEl.style.display !== "none") {
-      xRayQuickViewEl.style.display = "none";
-    }
-  });
+function hideXrayQuickView() {
+	const xRayQuickViewEls = document.querySelectorAll(".xrayQuickView");
+	xRayQuickViewEls.forEach((xRayQuickViewEl) => {
+		if (xRayQuickViewEl.style.display !== "none") {
+			xRayQuickViewEl.style.display = "none";
+		}
+	});
 
-  const overlayContainerFirstDivChildEls = document.querySelectorAll(
-    '[class*="-overlays-container"] > div'
-  );
+	const overlayContainerFirstDivChildEls = document.querySelectorAll(
+		'[class*="-overlays-container"] > div'
+	);
 
-  overlayContainerFirstDivChildEls.forEach(
-    (overlayContainerFirstDivChildEl) => {
-      if (overlayContainerFirstDivChildEl.style.background !== "none") {
-        overlayContainerFirstDivChildEl.style.background = "none";
-      }
-    }
-  );
+	overlayContainerFirstDivChildEls.forEach(
+		(overlayContainerFirstDivChildEl) => {
+			if (overlayContainerFirstDivChildEl.style.background !== "none") {
+				overlayContainerFirstDivChildEl.style.background = "none";
+			}
+		}
+	);
 
-  const captionsOverlayEls = document.querySelectorAll(
-    '[class*="-captions-overlay"]'
-  );
+	const captionsOverlayEls = document.querySelectorAll(
+		'[class*="-captions-overlay"]'
+	);
 
-  captionsOverlayEls.forEach((captionsOverlayEl) => {
-    if (captionsOverlayEl.style.opacity !== "1") {
-      captionsOverlayEl.style.opacity = "1";
-    }
-  });
+	captionsOverlayEls.forEach((captionsOverlayEl) => {
+		if (captionsOverlayEl.style.opacity !== "1") {
+			captionsOverlayEl.style.opacity = "1";
+		}
+	});
 };
 
 const observer = new MutationObserver(hideXrayQuickView);
 
 window.onload = function () {
-  const config = { childList: true, subtree: true };
-  observer.observe(document.body, config);
+	const config = { childList: true, subtree: true };
+	observer.observe(document.body, config);
 };
